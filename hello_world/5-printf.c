9a24b136759d9a9f14f125289c64306457d8074e
Lazherhedfi <lazherhedfi@live.fr>
2022-09-30 15:14:10 -0700

    prog that print

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - write a c program that prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
