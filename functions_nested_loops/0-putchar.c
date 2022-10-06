59cc52c580583c7db47d2d65ded3e44cf3719927
Lazherhedfi <lazherhedfi@live.fr>
2022-10-06 22:24:24 +0100

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+/**
+ * main - prints
+ * Return: 0
+ */
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
+
