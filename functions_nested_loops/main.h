4a132ec0e41e8336cda1df56988bf9ee50c50447
Lazherhedfi <113652009+Lazherhedfi@users.noreply.github.com>
2022-10-06 22:20:57 +0100

    header

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+#include <ctype.h>
+int _putchar(char c);
+void print_alphabet(void);
+int main(void);
+#endif
