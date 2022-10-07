3ad3a3653c903b78eaa334af3c580cfc2402f0fc
Lazherhedfi <lazherhedfi@live.fr>
2022-10-07 04:25:47 -0700

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print signs
+ * @i: int
+ * Return: 0
+ */
+int print_sign(int i)
+{
+	if (i > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (i < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
