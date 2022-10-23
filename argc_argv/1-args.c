682b63cf9238c9c491df0151c799471d1a7996e3
Lazherhedfi <lazherhedfi@live.fr>
2022-10-23 16:01:41 -0700

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the numver of arguments
+ * @argc: int
+ * @argv: int
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
